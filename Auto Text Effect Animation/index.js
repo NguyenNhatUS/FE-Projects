b1b5ec3547b22783a10d979be029a9ad65aea56a
NguyenNhatUS <nldnhat182006@gmail.com>
2025-06-02 17:19:57 +0700

    Add Auto Text Effect Animation

---
+++ Auto Text Effect Animation/index.js
@@ -0,0 +1,25 @@
+const containerElement = document.querySelector('.container');
+
+const careers = ['fresh man at HCMUS','Youtuber', 'Web developer', 'Java Developer'];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerElement.innerHTML = `<h1>Iam a ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+
+  if(characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if(careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
+
